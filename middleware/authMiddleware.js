687f6d8657bc976bf656bee9e7fe5d6043601cdd
Deepesh Sahu <sahudeepesh46@gmail.com>
2024-12-06 13:29:17 +0530

    db password is change

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const config = require('../config/config');
-
-// module.exports = function (req, res, next) {
-//   const token = req.header('Authorization')?.split(' ')[1];
-
-//   if (!token) {
-//     return res.status(401).json({status:'0', message: 'No token, authorization denied' });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, config.jwtSecret);
-//     req.user = decoded.user;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({status:'0', message: 'Token is not valid' });
-//   }
-// };
-
 
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
